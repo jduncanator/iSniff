2f50d3e8c70feaae83977cfb6b966540283890c3
jduncanator <jduncanator@hotmail.com>
2014-10-04 16:40:16 +1000

    Add Apple License to iftap.h

--- include/iftap.h
+++ include/iftap.h
@@ -1,3 +1,19 @@
+/*
+ * Extract from Apple XNU Kernel
+ * Copyright (c) 1999-2010 Apple Inc. All rights reserved.
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. The rights granted to you under the License
+ * may not be used to create, or enable the creation or redistribution of,
+ * unlawful or unlicensed copies of an Apple operating system, or to
+ * circumvent, violate, or enable the circumvention or violation of, any
+ * terms of an Apple operating system software license agreement.
+ */
+#ifndef IPTAP_H
+#define IPTAP_H
+
 #pragma pack(push)
 #pragma pack(1)
 
@@ -15,3 +31,5 @@ typedef struct iptap_hdr_t {
 } iptap_hdr_t;
 
 #pragma pack(pop)
+
+#endif /* IPTAP_H */
