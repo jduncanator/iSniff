42db9e58b0bdb5e860d2f0e7135f14070069385b
jduncanator <jduncanator@hotmail.com>
2014-10-04 22:03:03 +1000

    Refactored the source code now that it works
    Should now compile across platforms. Makefile still absent but now supports command line options. This refactoring is designed to handle memory a bit better and will dispose of things it previously didn't. It also handles SIGINT and SIGTERM to properly flush the pcap files to disk and free all remaining memory.

--- src/main.c
+++ src/main.c
@@ -20,102 +20,272 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 
  * USA
- */
-
-
+*/
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <Winsock2.h>
+#include <getopt.h>
+#include <signal.h>
+#include <time.h>
 
-#include <libimobiledevice/property_list_service.h>
-#include <libimobiledevice/libimobiledevice.h>
 #include <plist/plist.h>
+
+#include <libimobiledevice/libimobiledevice.h>
+#include <libimobiledevice/property_list_service.h>
 
 #include "pcap.h"
 #include "iftap.h"
 
+#ifdef WIN32
+#include <Winsock2.h>
+#else
+#include <netinet/in.h>
+#endif
+
+char *outfile = NULL;
+char *udid = NULL;
+
+static char list_udid = 0;
+static char quit_flag = 0;
+
+/* 
+ * str_is_udid from ideviceinstaller.c
+ * Copyright (C) 2010 Nikias Bassen <nikias@gmx.li>
+ * Licensed under the GNU General Public License Version 2
+ */
+static int str_is_udid(const char* str)
+{
+	const char allowed[] = "0123456789abcdefABCDEF";
+
+	/* handle NULL case */
+	if (str == NULL)
+		return -1;
+
+	int length = strlen(str);
+
+	/* verify length */
+	if (length != 40)
+		return -1;
+
+	/* check for invalid characters */
+	while(length--) {
+		/* invalid character in udid? */
+		if (strchr(allowed, str[length]) == NULL) {
+			return -1;
+		}
+	} 
+
+	return 0;
+}
+
+static void handle_interrupt(int sig)
+{
+	fprintf(stderr, "Closing...\n");
+	quit_flag = 1;
+}
+
+static void print_usage(int argc, char **argv)
+{
+	char *name = NULL;
+
+	name = strrchr(argv[0], '/');
+	printf("Usage: %s [OPTIONS] [PCAPFILE]\n", (name ? name + 1 : argv[0]));
+	printf("Capture packets on a connected iDevice.\n\n");
+	printf("  If PCAPFILE is passed, write the raw packets to file\n"
+		   "  rather than writing to STDOUT.\n\n");
+	printf
+		("  -u, --udid UDID\tTarget specific device by its 40-digit device UDID.\n"
+		 "  -l, --list\t\tlist UDID of all attached devices\n"
+		 "  -h, --help\t\tprints usage information\n"
+		 "  -d, --debug\t\tenable communication debugging\n" "\n");
+}
+
+static void parse_opts(int argc, char **argv)
+{
+	static struct option longopts[] = {
+		{"help", 0, NULL, 'h'},
+		{"udid", 0, NULL, 'u'},
+		{"list", 0, NULL, 'l'},
+		{"debug", 0, NULL, 'd'},
+		{NULL, 0, NULL, 0}
+	};
+	int c;
+
+	while (1) {
+		c = getopt_long(argc, argv, "hlu:d", longopts,
+						(int *) 0);
+		if (c == -1) {
+			break;
+		}
+
+		switch (c) {
+		case 'h':
+			print_usage(argc, argv);
+			exit(0);
+		case 'u':
+			if (str_is_udid(optarg) == 0) {
+				udid = strdup(optarg);
+				break;
+			}
+			fprintf(stderr, "[ERROR] Invalid UDID specified\n");
+			print_usage(argc, argv);
+			exit(2);
+			break;
+		case 'l':
+			list_udid = 1;
+			break;
+		case 'd':
+			idevice_set_debug_level(1);
+			break;
+		default:
+			print_usage(argc, argv);
+			exit(2);
+		}
+	}
+
+	outfile = argv[optind];
+
+	if (argc - optind > 1) {
+		print_usage(argc, argv);
+		exit(2);
+	}
+}
+
 int main (int argc, char *argv[])
 {
-	//printf("Sizeof(uint32): %i\n", sizeof(uint32_t));
+	parse_opts(argc, argv);
 
-	idevice_error_t ret = IDEVICE_E_UNKNOWN_ERROR;
+	idevice_error_t err = IDEVICE_E_UNKNOWN_ERROR;
 
 	idevice_t device = NULL;
-	ret = idevice_new(&device, NULL);
-	if (ret != IDEVICE_E_SUCCESS) {
-		//printf("No device found, is it plugged in?\n");
-		return -1;
-	}
-
-	char* udid = NULL;
-	idevice_get_udid(device, &udid);
-	if(udid) {
-		//printf("Connected to %s\n", udid);
-	}
-
 	lockdownd_client_t lockdown = NULL;
-	if (lockdownd_client_new_with_handshake(device, &lockdown, "isniff") != LOCKDOWN_E_SUCCESS) {
-		//printf("Could not connect to lockdownd.\n");
-		idevice_free(device);
+	lockdownd_service_descriptor_t service = NULL;
+	property_list_service_client_t pcap_client = NULL;
+
+	pcap_t *pd = NULL;
+	pcap_dumper_t *pdumper = NULL;
+
+	if(list_udid) {
+		int i;
+		char **devices = NULL;
+		char *device_name = NULL;
+
+		if (idevice_get_device_list(&devices, &i) < 0) {
+			fprintf(stderr, "[ERROR] Unable to retrieve device list!\n");
+			return -1;
+		}
+
+		for (i = 0; devices[i] != NULL; i++) {
+			idevice_new(&device, devices[i]);
+			if (!device) {
+				fprintf(stderr, "[ERROR] No device with UDID %s connected.\n", devices[i]);
+				return -2;
+			}
+			if (LOCKDOWN_E_SUCCESS != lockdownd_client_new(device, &lockdown, "iSniff")) {
+				idevice_free(device);
+				fprintf(stderr, "[ERROR] Connecting to device failed!\n");
+				return -2;
+			}
+
+			if ((LOCKDOWN_E_SUCCESS != lockdownd_get_device_name(lockdown, &device_name)) || !device_name) {
+				lockdownd_client_free(lockdown);
+				idevice_free(device);
+				fprintf(stderr, "[ERROR] Could not get device name!\n");
+				return -2;
+			}
+			
+			printf("%s - %s\n", devices[i], device_name);
+
+			if (device_name) {
+				free(device_name);
+			}
+		}
+
+		idevice_device_list_free(devices);
+		return 0;
+	}
+
+	err = idevice_new(&device, udid);
+	if (err != IDEVICE_E_SUCCESS) {
+		if (udid) {
+			printf("No device found with udid %s, is it plugged in?\n", udid);
+		} else {
+			printf("No device found, is it plugged in?\n");
+		}
 		return -1;
 	}
 
-	lockdownd_service_descriptor_t service = NULL;
-	ret = lockdownd_start_service(lockdown, "com.apple.pcapd", &service);
-	if (ret != LOCKDOWN_E_SUCCESS || !(service && service->port)) {
-		//printf("Could not start com.apple.pcapd.\n");
+	if(!udid) {
+		idevice_get_udid(device, &udid);
+	}
+
+	if(udid) {
+		fprintf(stderr, "Connected to %s\n", udid);
+		free(udid);
+	}
+	
+	if (LOCKDOWN_E_SUCCESS != lockdownd_client_new_with_handshake(device, &lockdown, "iSniff")) {
+		idevice_free(device);
+		printf("Could not connect to lockdownd.\n");
+		return -1;
+	}
+
+	err = lockdownd_start_service(lockdown, "com.apple.pcapd", &service);
+	if (err != LOCKDOWN_E_SUCCESS || !(service && service->port)) {
+		idevice_free(device);
+		printf("Could not start com.apple.pcapd\n");
 		return -1;
 	}
 	lockdownd_client_free(lockdown);
 
-	property_list_service_client_t client = NULL;
-	ret = property_list_service_client_new(device, service, &client);
-	if (ret != PROPERTY_LIST_SERVICE_E_SUCCESS) {
-		//printf("Could not connect to service %s! Port: %i, Err: %i\n", "com.apple.pcapd", service->port, ret);
+	err = property_list_service_client_new(device, service, &pcap_client);
+	if (err != PROPERTY_LIST_SERVICE_E_SUCCESS) {
+		idevice_free(device);
+		printf("Could not connect to service %s! Port: %i, Err: %i\n", "com.apple.pcapd", service->port, err);
 		return -1;
 	}
 	lockdownd_service_descriptor_free(service);
 
-	pcap_t *pd;
-    pcap_dumper_t *pdumper;
+	if(!outfile) {
+		outfile = "-";
+	}
 
-    pd = pcap_open_dead(DLT_EN10MB, 65535 /* snaplen */);
-    // Create the output file.
-    pdumper = pcap_dump_open(pd, "-");
+	pd = pcap_open_dead(DLT_EN10MB, 65535);
+	pdumper = pcap_dump_open(pd, outfile);
 
-	// Loop and receive all the packets
+	plist_t result = NULL;
+	iptap_hdr_t *tap_hdr = NULL;
+	struct pcap_pkthdr pcap_hdr;
+
+	signal(SIGINT, handle_interrupt);
+	signal(SIGTERM, handle_interrupt);
+
 	while(1) {
-		plist_t result = NULL;
-		char *plist_xml = NULL;
-		uint32_t size = 0;
-		property_list_service_receive_plist(client, &result);
-				
-		//plist_to_xml(result, &plist_xml, &size);
-		//fwrite(plist_xml, size, sizeof(char), stdout);
+		property_list_service_receive_plist(pcap_client, &result);
 
-		plist_type type = plist_get_node_type(result);
 		char* buff = NULL;
 		uint64_t length = 0;
 		plist_get_data_val(result, &buff, &length);
-
-		iptap_hdr_t *tap_hdr = (iptap_hdr_t*)buff;
-		//printf("Header length: %i\n", ntohl(tap_hdr->hdr_length));
-		//printf("Packet length: %i\n", ntohl(tap_hdr->length));
-
-		struct pcap_pkthdr hdr;
-		gettimeofday (&hdr.ts, NULL);
-		hdr.caplen = hdr.len = ntohl(tap_hdr->length);
-
-		//printf("Buffer pointer: 0x%i\nPacket pointer: 0x%i\n", buff, (buff + ntohl(tap_hdr->hdr_length)));
-		pcap_dump(pdumper, &hdr, (buff + ntohl(tap_hdr->hdr_length)));
-		pcap_dump_flush(pdumper);
 		plist_free(result);
+
+		tap_hdr = (iptap_hdr_t*)buff;
+		
+		gettimeofday(&pcap_hdr.ts, NULL);
+		pcap_hdr.caplen = ntohl(tap_hdr->length);
+		pcap_hdr.len = ntohl(tap_hdr->length);
+
+		pcap_dump((char*)pdumper, &pcap_hdr, (buff + ntohl(tap_hdr->hdr_length)));
+		pcap_dump_flush(pdumper);
+
+		free(buff);
+
+		if(quit_flag) break;
 	}
 
 	pcap_close(pd);
-    pcap_dump_close(pdumper);
+	pcap_dump_close(pdumper);
+	property_list_service_client_free(pcap_client);
+	idevice_free(device);
+
 	return 0;
 }
