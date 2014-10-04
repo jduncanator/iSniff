7df29ef69dec854572836ea90ad0c6823613bf81
jduncanator <jduncanator@hotmail.com>
2014-10-04 15:42:01 +1000

    Update license to be compatible with libimobiledevice

--- src/main.c
+++ src/main.c
@@ -1,3 +1,29 @@
+/*
+ * iSniff
+ * Command line interface to use a device's remote packet capture service
+ *
+ * Copyright (C) 2014 jduncanator
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more profile.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 
+ * USA
+ */
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
